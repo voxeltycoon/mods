c95d0db4ac35f8a5392fd03943aae935d6af6dec
Andrew Peysakhovich <andrewpey@live.ru>
2022-01-30 00:20:34 +0300

    SettingsExampleMod

---
+++ SettingsExampleMod/SettingsExampleMod.cs
@@ -0,0 +1,15 @@
+﻿using VoxelTycoon;
+using VoxelTycoon.Modding;
+using VoxelTycoon.UI.Windows;
+
+namespace SettingsExampleMod
+{
+    public class SettingsExampleMod : Mod
+    {
+        protected override void OnGameStarted()
+        {
+            var boolValue = WorldSettings.Current.GetBool<SettingsExampleMod>("ExampleBoolValue");
+            Dialog.ShowPositiveMessage(boolValue.ToString());
+        }
+    }
+}
