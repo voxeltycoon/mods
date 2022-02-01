398b8eeb20f8bbbe5925c6d763380b834eb00ed9
Andrew Peysakhovich <andrewpey@live.ru>
2022-02-01 23:52:47 +0300

    SettingsExampleMod updated for 0.87.1.2

---
+++ SettingsExampleMod/ExampleMod.cs
@@ -0,0 +1,16 @@
+﻿using VoxelTycoon;
+using VoxelTycoon.Modding;
+using VoxelTycoon.UI.Windows;
+
+namespace SettingsExampleMod
+{
+    public class ExampleMod : Mod
+    {
+        protected override void OnGameStarted()
+        {
+            var showDialog = WorldSettings.Current.GetBool<ExampleSettingsMod>(ExampleSettingsMod.ShowDialogKey);
+            if (showDialog)
+                Dialog.ShowPositiveMessage("Yay, it's working!");
+        }
+    }
+}
---
+++ SettingsExampleMod/ExampleSettingsMod.cs
@@ -0,0 +1,24 @@
+﻿using VoxelTycoon;
+using VoxelTycoon.Game.UI;
+using VoxelTycoon.Modding;
+
+namespace SettingsExampleMod
+{
+    public class ExampleSettingsMod : SettingsMod
+    {
+        public const string ShowDialogKey = "ShowDialog";
+
+        protected override void SetDefaults(WorldSettings worldSettings)
+        {
+            worldSettings.SetBool<ExampleSettingsMod>(ShowDialogKey, false);
+        }
+
+        protected override void SetupSettingsControl(SettingsControl settingsControl, WorldSettings worldSettings)
+        {
+            settingsControl.AddToggle("Show dialog", null,
+                worldSettings.GetBool<ExampleSettingsMod>(ShowDialogKey),
+                () => worldSettings.SetBool<ExampleSettingsMod>(ShowDialogKey, true),
+                () => worldSettings.SetBool<ExampleSettingsMod>(ShowDialogKey, false));
+        }
+    }
+}
